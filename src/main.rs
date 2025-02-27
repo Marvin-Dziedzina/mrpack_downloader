ca9c87d0f4fee620a56b556661ece0ff40f04915
Mixaffected <marvin.dziedzina@gmail.com>
2025-02-27 17:19:28 +0100

    Added command line args

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use clap::{Arg, Command, arg, command};
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+fn main() {
+    let matches = Command::new("MRPACK Downloader")
+        .version(VERSION)
+        .about("MRPACK DOWNLOADER downloads all mods from a .mrpack modpack")
+        .arg(
+            Arg::new("mrpack_path")
+                .long("mrpack_path")
+                .short('p')
+                .help("The path to the .mrpack modpack file"),
+        )
+        .arg(
+            Arg::new("out_path")
+                .long("out_path")
+                .short('o')
+                .help("The path where the out_mrpack directory gets created")
+                .default_value("./"),
+        )
+        .get_matches();
+}
