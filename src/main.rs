e138fc71dbb92393bcabbaf30274e495a2b5cc49
Mixaffected <marvin.dziedzina@gmail.com>
2025-03-01 22:41:23 +0100

    Updated

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use std::{
 };
 
 use clap::{Arg, Command};
-use log::{info, warn};
 use mrpack::MrPack;
 
 mod mrpack;
@@ -17,8 +16,6 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 const OUT_DIRECTORY: &str = "out_mrpack";
 
 fn main() -> Result<(), &'static str> {
-    env_logger::init();
-
     let matches = Command::new("MRPACK Downloader")
         .version(VERSION)
         .about("MRPACK DOWNLOADER downloads all mods from a .mrpack Modrinth modpack")
@@ -26,7 +23,8 @@ fn main() -> Result<(), &'static str> {
             Arg::new("mrpack_path")
                 .long("mrpack_path")
                 .short('p')
-                .help("The path to the .mrpack unpacked modpack file"),
+                .help("The path to the .mrpack unpacked modpack file")
+                .required(true),
         )
         .arg(
             Arg::new("out_path")
@@ -37,7 +35,7 @@ fn main() -> Result<(), &'static str> {
         )
         .get_matches();
 
-    info!("Preparing paths");
+    println!("Preparing paths");
 
     let mrpack_path_string = matches.get_one::<String>("mrpack_path").unwrap();
     let mrpack_path = PathBuf::from(mrpack_path_string);
@@ -48,22 +46,22 @@ fn main() -> Result<(), &'static str> {
     out_path.push(OUT_DIRECTORY);
     fs::create_dir_all(&out_path).expect("Failed to create all folders that are missing");
 
-    info!("Created directories");
+    println!("Created directories");
 
-    info!("Check if mrpack exists");
+    println!("Check if mrpack exists");
     if !fs::exists(&mrpack_path).expect("Failed to verify that mrpack path exists") {
         return Err("Failed to find MRPACK file");
     };
 
-    info!("Mrpack exists");
+    println!("Mrpack exists");
 
     let mrpack = get_mrpack(&mrpack_path);
-    info!("Deserialization of mrpack done");
+    println!("Deserialization of mrpack done");
 
     let mut successful_downloaded: Vec<String> = Vec::new();
     let mut failed_downloads: Vec<String> = Vec::new();
 
-    info!("Starting to download mods...");
+    println!("Starting to download mods...");
     for file in mrpack.files.iter() {
         let mod_name: &str = match file
             .path
@@ -78,17 +76,17 @@ fn main() -> Result<(), &'static str> {
         let mut mod_path = out_path.clone();
         mod_path.push(mod_name);
 
-        info!("Downloading {}...", mod_name);
+        println!("Downloading {}...", mod_name);
 
         let mut download_failed = false;
         for download_link in file.downloads.iter() {
-            info!("Downloading from {}", download_link);
+            println!("Downloading from {}", download_link);
 
             let mod_data = match reqwest::blocking::get(download_link) {
                 Ok(mod_data) => mod_data,
                 Err(e) => {
                     download_failed = true;
-                    warn!(
+                    println!(
                         "Failed to download {} from link \"{}\" with error: {}",
                         mod_name, download_link, e
                     );
@@ -100,7 +98,7 @@ fn main() -> Result<(), &'static str> {
                 Ok(file) => file,
                 Err(e) => {
                     download_failed = true;
-                    warn!("{} failed to install with error: {}", mod_name, e);
+                    println!("{} failed to install with error: {}", mod_name, e);
                     continue;
                 }
             };
@@ -108,7 +106,7 @@ fn main() -> Result<(), &'static str> {
                 Ok(bytes) => bytes,
                 Err(e) => {
                     download_failed = true;
-                    warn!("{} failed to get bytes with error: {}", mod_name, e);
+                    println!("{} failed to get bytes with error: {}", mod_name, e);
                     continue;
                 }
             };
@@ -117,12 +115,12 @@ fn main() -> Result<(), &'static str> {
                 Ok(_) => (),
                 Err(e) => {
                     download_failed = true;
-                    warn!("{} failed to write to drive with error: {}", mod_name, e);
+                    println!("{} failed to write to drive with error: {}", mod_name, e);
                     continue;
                 }
             };
 
-            info!("{} sucessfully downloaded!", mod_name);
+            println!("{} sucessfully downloaded!", mod_name);
             successful_downloaded.push(mod_name.to_owned());
             break;
         }
@@ -132,36 +130,33 @@ fn main() -> Result<(), &'static str> {
         };
     }
 
-    info!(
+    println!(
         "Successfully installed {} from {} mods",
         successful_downloaded.len(),
         successful_downloaded.len() + failed_downloads.len()
     );
 
-    {
-        let mut successful_mods: String = String::new();
-        for success_mod in successful_downloaded.iter() {
-            successful_mods.push_str(success_mod);
-            successful_mods.push('\n');
-        }
-        info!(
-            "Successful downloaded mods {}:\n{}",
-            successful_downloaded.len(),
-            successful_mods
-        );
+    let mut successful_mods: String = String::new();
+    for success_mod in successful_downloaded.iter() {
+        successful_mods.push_str(success_mod);
+        successful_mods.push('\n');
     }
-    {
-        let mut failed_mods: String = String::new();
-        for failed_mod in failed_downloads.iter() {
-            failed_mods.push_str(failed_mod);
-            failed_mods.push('\n');
-        }
-        info!(
-            "Failed downloaded mods {}:\n{}",
-            failed_downloads.len(),
-            failed_mods
-        );
+    println!(
+        "Successful downloaded mods {}:\n{}",
+        successful_downloaded.len(),
+        successful_mods
+    );
+
+    let mut failed_mods: String = String::new();
+    for failed_mod in failed_downloads.iter() {
+        failed_mods.push_str(failed_mod);
+        failed_mods.push('\n');
     }
+    println!(
+        "Failed downloaded mods {}:\n{}",
+        failed_downloads.len(),
+        failed_mods
+    );
 
     Ok(())
 }
